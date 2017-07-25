de1ddc887e8f9c65659842965a86d3a1b0f9da92
Ian D. McCunn <imccunn@expedia.com>
2017-07-25 11:30:26 -0700

    Cleanup.

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ import { postDecipher } from './src/postCrypt';
 
 const args = process.argv;
 const secret = fs.readFileSync('./.secret', 'utf8');
-
-let actOpt = args[2];
-let source = args[3];
-let dest = args[4];
-let searchTerm = args[5];
-let searchTerm2 = args[6];
+const actOpt = args[2];
+const source = args[3];
+const dest = args[4];
+const searchTerm = args[5];
+const searchTerm2 = args[6];
 
 const ENC = '-e';
 const DEC = '-d';
